5084af91be9dc95aba4334e1a6269fe6105ad54a
toshioutsumi-lab <toshio.utsumi@gmail.com>
2026-04-09 11:25:19 +0900

    Create masterPerfumes array with initial data
    Added master perfume data with 8 entries.

---
+++ data.js
@@ -0,0 +1,12 @@
+// 香水マスターデータ（20個、100個と増やせます）
+const masterPerfumes = [
+    { id: 0, name: 'ブルガリ プールオム', img: 'images/Bvlgari-Pour-Homme-min.png', type: 'clean', style: 'minimal' },
+    { id: 1, name: 'LV イマジナシオン', img: 'images/Imagination-min-1.png', type: 'luxury', style: 'luxury' },
+    { id: 2, name: 'Dior ソヴァージュ', img: 'images/dior-sauvage-min.png', type: 'power', style: 'luxury' },
+    { id: 3, name: 'レイジーサンデーモーニング', img: 'images/replica-lazy-sunday-morning-min.png', type: 'clean', style: 'minimal' },
+    { id: 4, name: 'クリード アバントゥス', img: 'images/creed-aventus-min.jpg', type: 'power', style: 'luxury' },
+    { id: 5, name: 'ジョーマローン イングリッシュペア', img: 'images/jomalone-english-pear-and-freesia-min.jpg', type: 'clean', style: 'minimal' },
+    { id: 6, name: 'ワンダーウッド', img: 'images/wonderwood-min.png', type: 'luxury', style: 'art' },
+    { id: 7, name: 'エンディミオン コンサントレ', img: 'images/penhaligon-endymion-intense-min.png', type: 'luxury', style: 'art' }
+    // 新しい香水を追加するときは、ここに行を増やしていくだくだけ！
+];
